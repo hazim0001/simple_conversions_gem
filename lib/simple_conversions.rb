39fce775d5c996f2a14fa0dfc76070bfc48548f0
hazim0001 <hazim_alsayed@hotmail.com>
2020-12-25 13:54:08 +0200

    first commit

---
+++ lib/simple_conversions.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "simple_conversions/version"
+
+module SimpleConversions
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/simple_conversions/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SimpleConversions
+  VERSION = "0.1.0"
+end
